6a4c4f04dc67ee8fc79d9df0524d73454a6b6637
Robel Amde <Robelamde21@gmail.com>
2022-05-31 12:33:57 -0700

    2-Task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z") + 1):
-    if chr(i) != "q" and chr (i) != "e":
-        print("{:s}".format(chr(i)), end="")
+    print("{:s}".format(chr(i)), end="")
