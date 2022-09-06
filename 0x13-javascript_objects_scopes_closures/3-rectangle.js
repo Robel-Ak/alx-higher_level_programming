8beb1bacdd7c49c6d7d53c9348ca828b1ac8f5f0
Robel-Ak <Robelamde21@gmail.com>
2022-09-06 22:22:43 +0300

    Task

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -6,10 +7,9 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height).split('')
+      .slice(0, -1).join(''));
   }
 };
-
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -6,16 +7,18 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height).split('')
+      .slice(0, -1).join(''));
   }
+
   rotate () {
-    let tmp = this.width;
+    const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
