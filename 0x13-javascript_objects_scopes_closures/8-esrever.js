4c8f7201a067bc40417ab9b0be2749bd311a79b1
Robel-Ak <Robelamde21@gmail.com>
2022-09-06 22:26:53 +0300

    Task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
+
 exports.esrever = function (list) {
-  let rev = [];
-  for (let i = 0; i < list.length; i++) {
-    rev.unshift(list[i]);
-  }
-  return rev;
+  return list.sort(() => 1);
 };
