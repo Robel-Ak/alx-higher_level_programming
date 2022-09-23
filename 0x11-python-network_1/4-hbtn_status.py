9149a1137b0f45bd1aff23a0da5468df13adc75a
Robel-Ak <Robelamde21@gmail.com>
2022-09-23 16:49:29 +0300

    Task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
-if __name__ == '__main__':
-    import requests
-    r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
+"""Check status"""
+import requests
+
+
+def status():
+    """status"""
+    result = requests.get("https://intranet.hbtn.io/status")
+
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(result.text)))
+    print("\t- content: {}".format(result.text))
+
+if __name__ == "__main__":
+    status()
